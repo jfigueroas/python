833d1c010f7195aa774a0dc0d025a3778c9e6d92
jfigueroas <61071222+jfigueroas@users.noreply.github.com>
2020-03-07 12:45:17 -0500

    Add files via upload

---
+++ modulosMath.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 22 09:57:58 2020
+
+@author: CEC
+"""
+
+import math as m 
+
+print(m.sin(m.pi/5))
---
+++ multiplicacionJlf.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 21 18:55:32 2020
+
+@author: CEC
+"""
+
+def multiply(a,b):
+    return a*b
+
+print(multiply(3,4)) #outputs 12 
+
+def multiply(a,b):
+    return 
+
+print (multiply(3,4))
